824669fe60f6d90ce86a8d3d2cd206a7601340ce
stopachka <stepan.p@gmail.com>
2016-10-27 11:09:02 +0200

    sm

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from plot import plot_perceptron
 # -----------------------------------------------------------------------------
 # learn
 
-def load_data(name):
-    return sio.loadmat('coursera/Datasets/' + name)
-
 def add_bias(examples):
     return np.column_stack([examples, np.ones(examples.shape[0])])
 
@@ -70,7 +67,8 @@ def display(neg_examples, pos_examples, w, feasible, error_history, weight_dist_
     (neg_mistakes, pos_mistakes) = eval_weight(neg_examples, pos_examples, w)
 
     error_history = error_history + [len(neg_mistakes) + len(pos_mistakes)]
-    weight_dist_history = weight_dist_history + [np.linalg.norm(w - feasible)]
+    if any(feasible):
+        weight_dist_history = weight_dist_history + [np.linalg.norm(w - feasible)]
 
     print 'w: ', w, 'neg_mistakes:', neg_mistakes, 'pos_mistakes: ', pos_mistakes
 
@@ -83,7 +81,7 @@ def display(neg_examples, pos_examples, w, feasible, error_history, weight_dist_
 # init
 
 def init():
-    dataset = load_data('dataset1.mat')
+    dataset = sio.loadmat('coursera/Datasets/dataset1.mat')
 
     neg_examples = add_bias(dataset['neg_examples_nobias'])
     pos_examples = add_bias(dataset['pos_examples_nobias'])
