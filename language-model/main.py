ef320fcce3c4d80a95591d09ba46ab40e6a1b1a7
stopachka <stepan.p@gmail.com>
2016-10-29 17:05:34 +0200

    data

--- language-model/main.py
+++ language-model/main.py
@@ -2,10 +2,19 @@ import numpy as np
 import scipy.io as sio
 
 # -----------------------------------------------------------------------------
+# transform data
+
+def batch(dataset):
+    sequences = {key: dict() for key in ['train', 'valid', 'test']}
+    print sequences
+
+
+# -----------------------------------------------------------------------------
 # init
 
 def init():
     dataset = sio.loadmat('coursera/data.mat')['data']
+    sequences = batch(dataset)
 
 if __name__ == '__main__':
     init()
